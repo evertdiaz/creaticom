c5376137fe42e83ca65c9a2a847a6903b1ef0cb8
Evert Diaz <evertdiazb@gmail.com>
2017-05-24 17:56:55 -0500

    Agregada vista segun categorias y subcategorias

--- routes/obra.js
+++ routes/obra.js
@@ -9,6 +9,18 @@ router.get('/all', function (req, res) {
     res.render('obra/all', { title: 'Obras', data: response.body })
   })
 })
+router.get('/category/:name', (req, res) => {
+  request(apiURL + '/obras', {json: req.body}, (error, response, body) => {
+    res.render('obra/category', {title: 'Obras', data: body, categoria: req.params.name})
+  })
+})
+
+router.get('/subcategory/:name', (req, res) => {
+  request(apiURL + '/obras', {json: req.body}, (error, response, body) => {
+    res.render('obra/subcategory', {title: 'Obras', data: body, subcategoria: req.params.name})
+  })
+})
+
 router.get('/:id', function (req, res) {
   request(apiURL + '/obra/' + req.params.id, {json: req.body}, (error, response, body) => {
     if (error) return res.send(error)
