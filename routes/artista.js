ebbe3711babd6d1c721662174c111e73633162bb
Evert Diaz <evertdiazb@gmail.com>
2017-05-24 19:05:35 -0500

    Perfil publico avanzado

--- routes/artista.js
+++ routes/artista.js
@@ -12,9 +12,17 @@ router.get('/all', (req, res) => {
 
 router.get('/:username', (req, res) => {
   request(apiURL + '/artista/' + req.params.username, {json: req.body}, (error, response, body) => {
-    if (error) return res.send(error)
-    if(response.body.isArtist) res.render('artista/single', { data: response.body, title: 'Artista ' + req.params.username })  
-    else res.render('artista/single', { data: {alert: 'No existe Artista'}, title: 'Artista ' + req.params.username })  
+    console.log('+++++++', body)
+    if (body == undefined) return res.render('artista/single', { data: {alert: 'No existe Artista'}, title: 'Artista ' + req.params.username, categorias:null, subcategorias:null, obras:null } )  
+    if (!body.isArtist) return res.render('artista/single', { data: {alert: 'No existe Artista'}, title: 'Artista ' + req.params.username, categorias:null, subcategorias:null, obras:null } )  
+    request(apiURL + '/obrasfull', {json:req.body}, (err1, resp1, body1) => {
+      request(apiURL + '/categoriasfull', {json:req.body}, (err2, resp2, body2) => {
+        request(apiURL + '/subcategoriasfull', {json:req.body}, (err3, resp3, body3) => {
+          if (error) return res.send(error)
+          res.render('artista/single', { data: response.body, title: 'Artista ' + req.params.username, categorias:body2, subcategorias:body3, obras:body1 } )  
+        })
+      })
+    })
   })
 })
 
